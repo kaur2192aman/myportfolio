3008d1c48e7a81e94db809d5210edd75e6eccf17
Amanpreet kaur <kaur.2192aman@gmail.com>
2023-09-17 15:45:33 -0400

    change in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,33 +8,35 @@ import { faFacebookF, faGithub, faLinkedin, fab } from '@fortawesome/free-brands
 library.add(fas, far, fab)
 const Home = () => {
     return (
-
-      <>
-      
-          <section className="resume-section" id="about">
-                    <div className="resume-section-content">
-                        <h1 className="mb-0">
-                            Amanpreet
-                            <span className="text-primary">Kaur</span>
-                        </h1>
-                        <div className="subheading mb-5">
-                            <p><a href="tel:+1(647) 573-5092"><FontAwesomeIcon icon={faPhone} />  (647) 573-5092 </a></p>
-                            <p><a href="mailto:amanpreet.kaur.ka@gmail.com">amanpreet.kaur.ka@gmail.com</a></p>
-                        </div>
-    
-                           
-                        <p className="lead mb-5">I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. I enjoy being challenged and engaging with projects that require me to work outside my comfort and knowledge set, as continuing to learn new languages and development techniques are important to me and the success of your organization.
-                        </p>
-                        <div className="social-icons">
-                            <a className="social-icon" href="https://www.linkedin.com/in/kaur2192aman/" target='_   blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
-                            <a className="social-icon" href="https://github.com/kaur2192aman/portfolio.git" target="_blank"  rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
-                            {/* <a className="social-icon" href="#!"><FontAwesomeIcon icon={faTwitter} /></a> */}
-                            <a className="social-icon" href="https://www.facebook.com/kaur.2192aman/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebookF} /></a>
-                        </div>
+        <>
+            <section className="resume-section" id="about">
+                <div className="resume-section-content">  
+                    <div class="row">
+                    <div class="col-md-4 how-img">
+                            <img src="/profile.jpeg" class=" img-fluid" alt="" />
                     </div>
-                </section>
-                </>
-        );
-      };
+                    <div class="col-md-8">
+                    <h1 className="">
+                        Amanpreet
+                        <span className="text-primary">Kaur</span>
+                    </h1>
+                    <div className="subheading mb-6">
+                        <p><a href="tel:+1(647) 573-5092"><FontAwesomeIcon icon={faPhone} />  (647) 573-5092 </a></p>
+                        <p><a href="mailto:amanpreet.kaur.ka@gmail.com">amanpreet.kaur.ka@gmail.com</a></p>
+                    </div>
+                        <p>I am experienced in leveraging agile frameworks to provide a robust synopsis for high level overviews. I enjoy being challenged and engaging with projects that require me to work outside my comfort and knowledge set, as continuing to learn new languages and development techniques are important to me and the success of your organization.</p>   
+                    </div>
+                </div>
+                        
+                <div className="social-icons text-center">
+                    <a className="social-icon" href="https://www.linkedin.com/in/kaur2192aman/" target='_   blank' rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
+                    <a className="social-icon" href="https://github.com/kaur2192aman/portfolio.git" target="_blank"  rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
+                    <a className="social-icon" href="https://www.facebook.com/kaur.2192aman/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faFacebookF} /></a>
+                </div>
+                </div>
+            </section>
+        </>
+     );
+  };
   
   export default Home;
